8dcd70f0c8063bc71f18d81084d5cb9202d61243
dawidlehai <haidawidle@gmail.com>
2023-04-29 16:53:21 +0200

    feat: add binary search function

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+/*
+ OlogN
+ only for sorted data
+*/
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[mid] === needle) return true;
+        if (haystack[mid] > needle) hi = mid;
+        if (haystack[mid] < needle) lo = mid + 1;
+    } while (lo < hi);
+
+    return false;
 }
