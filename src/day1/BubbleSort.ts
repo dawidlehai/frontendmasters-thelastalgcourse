ff2ea32a18641eca9ab2c9e9a7a02bf4fcc0a0c9
dawidlehai <haidawidle@gmail.com>
2023-05-01 09:14:31 +0200

    feat: implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
-export default function bubble_sort(arr: number[]): void {
+/*
+  - you need to compare each pairs
+  - after each iteration there will be one less pair to compare as the highest number is in its place
+  - running time O(n^2)
+*/
 
+export default function bubble_sort(arr: number[]): void {
+    for (let n = arr.length - 1; n > 1; n--)
+        for (let i = 0; i < n; i++)
+            if (arr[i] > arr[i + 1])
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
 }
